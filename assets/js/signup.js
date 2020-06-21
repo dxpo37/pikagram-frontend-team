7babd56c570183a2fcc562d7513936a043de7881
Deepak Ponnuswamy <deepak.ponnuswamy@gmail.com>
2020-06-21 06:05:24 -0500

    sign up page is mostly functional

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,143 +1,108 @@
-import {databaseInputFieldChecker, inputFieldChecker} from "./utils.js"
+import {databaseInputFieldChecker, inputFieldChecker, changeColors, placeholderMovement} from "./utils.js"
 
-const emailElement = document.querySelector(".signinBox__emailInput")
-const fullnameElement  = document.querySelector(".signinBox__fullnameInput")
-const usernameElement  = document.querySelector(".signinBox__usernameInput")
-const passwordElement = document.querySelector(".signinBox__passwordInput")
+const emailElement = document.querySelector(".signupBox__emailInput")
+const firstnameElement  = document.querySelector(".signupBox__firstnameInput")
+const lastnameElement  = document.querySelector(".signupBox__lastnameInput")
+const usernameElement  = document.querySelector(".signupBox__usernameInput")
+const passwordElement = document.querySelector(".signupBox__passwordInput")
 
-const emailPlaceholder = document.querySelector(".signinBox__emailPlaceholder")
-const fullnamePlaceholder = document.querySelector(".signinBox__fullnamePlaceholder")
-const passwordPlaceholder = document.querySelector(".signinBox__passwordPlaceholder")
-const usernamePlaceholder = document.querySelector(".signinBox__usernamePlaceholder")
+const emailPlaceholder = document.querySelector(".signupBox__emailPlaceholder")
+const firstnamePlaceholder = document.querySelector(".signupBox__firstnamePlaceholder")
+const lastnamePlaceholder = document.querySelector(".signupBox__lastnamePlaceholder")
+const usernamePlaceholder = document.querySelector(".signupBox__usernamePlaceholder")
+const passwordPlaceholder = document.querySelector(".signupBox__passwordPlaceholder")
 
-const emailContainer = document.querySelector(".signinBox__emailFieldContainer")
-const fullnameContainer = document.querySelector(".signinBox__fullnameFieldContainer")
-const usernameContainer = document.querySelector(".signinBox__usernameFieldContainer")
-const passwordContainer = document.querySelector(".signinBox__passwordFieldContainer")
+const emailStatusImage = document.querySelector(".signupBox__emailStatusImage")
+const firstnameStatusImage = document.querySelector(".signupBox__firstnameStatusImage")
+const lastnameStatusImage = document.querySelector(".signupBox__lastnameStatusImage")
+const usernameStatusImage = document.querySelector(".signupBox__usernameStatusImage")
+const passwordStatusImage = document.querySelector(".signupBox__passwordStatusImage")
 
-const emailStatusImage = document.querySelector(".signinBox__emailStatusImage")
-const fullnameStatusImage = document.querySelector(".signinBox__fullnameStatusImage")
-const usernameStatusImage = document.querySelector(".signinBox__usernameStatusImage")
-const passwordStatusImage = document.querySelector(".signinBox__passwordStatusImage")
+const emailText = document.querySelector(".signupBox__emailImageText")
+const firstnameText = document.querySelector(".signupBox__firstnameImageText")
+const lastnameText = document.querySelector(".signupBox__lastnameImageText")
+const usernameText = document.querySelector(".signupBox__usernameImageText")
+const passwordText = document.querySelector(".signupBox__passwordImageText")
 
-const emailText = document.querySelector(".signinBox__emailImageText")
-const usernameText = document.querySelector(".signinBox__usernameImageText")
-const fullnameText = document.querySelector(".signinBox__fullnameImageText")
-const passwordText = document.querySelector(".signinBox__usernameImageText")
-
-const buttons = document.querySelectorAll(".signinBox__buttons")
-const signinButton = document.querySelector(".signupButton")
-const showButton = document.querySelector(".signinShowBtn")
-const errorText = document.querySelector(".signinBox__errorText")
+const signupButton = document.querySelector(".signupButton")
+const showButton = document.querySelector(".signupShowBtn")
+const errorText = document.querySelector(".signupBox__errorText")
 
 //query database and display icon indicating username or email availability
 window.addEventListener('click', (e) => { 
   databaseInputFieldChecker(emailElement, emailStatusImage, emailText, 'email', e)
   databaseInputFieldChecker(usernameElement, usernameStatusImage, usernameText, 'username', e)
-  inputFieldChecker(fullnameElement,fullnameStatusImage, fullnameText, e)
-  // inputFieldChecker(passwordElement,passwordStatusImage,e)
+  inputFieldChecker(firstnameElement,firstnameStatusImage, firstnameText, e)
+  inputFieldChecker(lastnameElement,lastnameStatusImage, lastnameText, e)
 });
 
+window.addEventListener('keyup', (e) => { 
+  if(e.key==="Tab"){
+  databaseInputFieldChecker(emailElement, emailStatusImage, emailText, 'email', e)
+  databaseInputFieldChecker(usernameElement, usernameStatusImage, usernameText, 'username', e)
+  inputFieldChecker(firstnameElement,firstnameStatusImage, firstnameText, e)
+  inputFieldChecker(lastnameElement,lastnameStatusImage, lastnameText, e)
+  }
+});
 
 //removed text from username field on reload
-window.addEventListener("load", ()=> {
-  emailElement.value = "" 
-  fullnameElement.value = "" 
-  usernameElement.value = "" 
-  passwordElement.value = "" 
-
-
-})
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+window.addEventListener("load", ()=> { emailElement.value = "" ;  firstnameElement.value = "" ; lastnameElement.value = "" ; usernameElement.value = "" ;  passwordElement.value = "" })
 
 //highlights signin button when password+username is correct length
 passwordElement.addEventListener("keyup", (e)=>{
   const passwordLength = passwordElement.value.length
-  const fullnameLength = fullnameElement.value.length
-
-  if(passwordLength>=1&&passwordLength<5){
-    passwordStatusImage.classList.add("tooshort")
-    passwordStatusImage.addEventListener("mouseleave" , ()=> {imageLabel.style.visibility="hidden" })
-    passwordStatusImage.addEventListener("mouseenter" , ()=> {
-      imageLabel.style.visibility="visible"
-      imageLabel.style.color="grey" 
-      imageLabel.innerHTML="too short"
-    })
-  }
-
-  else if(passwordLength>5) passwordStatusImage.classList.remove("tooshort"); passwordStatusImage.classList.add("available") 
+  const firstnameLength = firstnameElement.value.length
+  const lastnameLength = lastnameElement.value.length
 
   if(passwordLength>5 && 
-    fullnameLength>5 && 
+    firstnameLength>5 && 
+    lastnameLength>5  &&
     usernameStatusImage.classList.contains("available") && 
-    emailStatusImage.classList.contains("available")) signinButton.removeAttribute('disabled')
-  else  signinButton.setAttribute('disabled', true)
-})
+    emailStatusImage.classList.contains("available")) signupButton.removeAttribute('disabled')
+  else  signupButton.setAttribute('disabled', true)
 
-
-// //display showbox for password and move placeholder when password is entered
-// passwordElement.addEventListener("keyup", e=>{
-//   const passwordLength = e.target.value.length
-//   if(passwordLength>0) {
-//     showButton.classList.remove('signinShowBtn--hidden')
-//     passwordPlaceholder.classList.add("signinBox__passwordPlaceholder--active")
-//     passwordPlaceholder.classList.remove("signinBox__passwordPlaceholder--inactive")
-//   }
-//   else {
-//     showButton.classList.add('signinShowBtn--hidden')
-//     passwordPlaceholder.classList.remove("signinBox__passwordPlaceholder--active")
-//     passwordPlaceholder.classList.add("signinBox__passwordPlaceholder--inactive")
-//   }
-// })
-
-//move placeholder when username is entered
-
-usernameElement.addEventListener("keyup", e=>{
-  const usernameLength = e.target.value.length
-  if(usernameLength>0) {
-    usernamePlaceholder.classList.add("signinBox__usernamePlaceholder--active")
-    usernamePlaceholder.classList.remove("signinBox__usernamePlaceholder--inactive")
-  }
-  else {
-    usernamePlaceholder.classList.remove("signinBox__usernamePlaceholder--active")
-    usernamePlaceholder.classList.add("signinBox__usernamePlaceholder--inactive")
+  if(passwordLength>=1&&passwordLength<5) changeColors(passwordStatusImage, passwordText, "tooshort", "grey", "too short" )
+  else if(passwordLength>=5) {
+    passwordStatusImage.classList.remove("tooshort")
+    changeColors(passwordStatusImage, passwordText, "available", "green", "acceptable" ) 
   }
 })
 
-// //move input field for username when user starts to type
-// usernameElement.addEventListener("keydown", e=>{
-//   const usernameLength = e.target.value.length
-//   const key = e.key
-//   if(usernameLength === 1  ) {
-//     if(usernameElement.selectionStart === 0 && key === "Delete" || usernameElement.selectionStart === 1 && key === "Backspace")  
-//     usernameElement.classList.remove("signinBox__inputText--active")   
-//   }
-//   else {
-//     usernameElement.classList.add("signinBox__inputText--active")
-//   }
-// })
+//display showbox for password and move placeholder when password is entered
+passwordElement.addEventListener("keyup", e=>{
+  const passwordLength = e.target.value.length
+  if(passwordLength>0) showButton.classList.remove('signinShowBtn--hidden')
+  else showButton.classList.add('signinShowBtn--hidden')
+})
 
-// //move input field for password when user starts to type
-// passwordElement.addEventListener("keydown", e=>{
-//   const passwordLength = e.target.value.length
-//   const key = e.key
-//   if(passwordLength === 1  ) {
-//     if(passwordElement.selectionStart === 0 && key === "Delete" || passwordElement.selectionStart === 1 && key === "Backspace")  
-//     passwordElement.classList.remove("signinBox__inputText--active")   
-//   }
-//   else {
-//     passwordElement.classList.add("signinBox__inputText--active")
-//   }
-// })
+//move placeholder 
+placeholderMovement(emailElement, emailPlaceholder)
+placeholderMovement(firstnameElement, firstnamePlaceholder)
+placeholderMovement(lastnameElement, lastnamePlaceholder)
+placeholderMovement(usernameElement, usernamePlaceholder)
+placeholderMovement(passwordElement, passwordPlaceholder)
 
-// //changes text of show button when clicked
-// showButton.addEventListener('click', e=>{
-//   if(passwordElement.getAttribute("type")==="password") {passwordElement.setAttribute("type", "text"); showButton.innerHTML="Hide"}
-//   else {passwordElement.setAttribute("type", "password");; showButton.innerHTML="Show"}
-// })
+//changes text of show button when clicked
+showButton.addEventListener('click', e=>{
+  if(passwordElement.getAttribute("type")==="password") {passwordElement.setAttribute("type", "text"); showButton.innerHTML="Hide"}
+  else {passwordElement.setAttribute("type", "password");; showButton.innerHTML="Show"}
+})
 
-emailContainer.addEventListener('click', e=> {emailElement.focus()})
-fullnameContainer.addEventListener('click', e=> {fullnameElement.focus()})
-usernameContainer.addEventListener('click', e=> {usernameElement.focus()})
-passwordContainer.addEventListener('click', e=> {passwordElement.focus()})
+signupButton.addEventListener('click', async ()=>{
+  const res = await fetch(`https://cryptic-river-74579.herokuapp.com/api/users/`, {
+    method:"POST", 
+    headers: {'Content-Type': 'application/json'},
+    body:JSON.stringify({
+      "email":`${emailElement.value}`,
+      "firstName":`${firstnameElement.value}`,
+      "lastName":`${lastnameElement.value}`,
+      "userName":`${usernameElement.value}`,
+      "password":`${passwordElement.value}`,
+    })
+  })
+  const response = await res.json()
+  localStorage.token = response.token
+  localStorage.id = response.id
+  window.location.href = '/'
+})
+
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,4 @@
-export {login, databaseInputFieldChecker, inputFieldChecker}
+export {login, databaseInputFieldChecker, inputFieldChecker, changeColors, placeholderMovement}
 
 const login = async (username, password, error) => {
   const credentials = { email: username || username.value, password: password || password.value }
@@ -19,17 +19,17 @@ const login = async (username, password, error) => {
     localStorage.setItem('id', user.id)
     window.location.href = '/'
 }
-//Will find if username or password is already in use
+let changeColors = (imageElement, imageLabel, className, color, text)=> {
+  imageElement.classList.add(className) 
+  imageElement.addEventListener("mouseleave" , ()=> {imageLabel.style.visibility="hidden" })
+  imageElement.addEventListener("mouseenter" , ()=> {
+    imageLabel.style.visibility="visible" 
+    imageLabel.style.color=color 
+    imageLabel.innerHTML=text
+  })
+}
+//Will check if username or password is already in use
 const databaseInputFieldChecker = async (targetElement, imageElement, imageLabel, type, e)=> {
-  let changeColors = (className, color, text)=> {
-    imageElement.classList.add(className) 
-    imageElement.addEventListener("mouseleave" , ()=> {imageLabel.style.visibility="hidden" })
-    imageElement.addEventListener("mouseenter" , ()=> {
-      imageLabel.style.visibility="visible" 
-      imageLabel.style.color=color 
-      imageLabel.innerHTML=text
-    })
-  }
   if (targetElement.contains(e.target)) {
     imageElement.classList.remove("available");  imageElement.classList.remove("inuse");  imageElement.classList.remove("tooshort")
     imageLabel.innerHTML=""
@@ -43,9 +43,9 @@ const databaseInputFieldChecker = async (targetElement, imageElement, imageLabel
     const response = await res.json()
 
     //if username/email is available in db
-    if(response.message===true && targetElement.value.length>5) changeColors("available", "green", "available" )
-    else if (response.title==="User found" && targetElement.value.length>5 ) changeColors("inuse", "red", "in use" )
-    else if (targetElement.value.length>=1) changeColors("tooshort", "grey", "too short" )
+    if(response.message===true && targetElement.value.length>5) changeColors(imageElement, imageLabel, "available", "green", "available" )
+    else if (response.title==="User found" && targetElement.value.length>5 ) changeColors(imageElement, imageLabel, "inuse", "red", "in use" )
+    else if (targetElement.value.length>=1) changeColors(imageElement, imageLabel, "tooshort", "grey", "too short" )
   }
 }
 
@@ -55,14 +55,33 @@ const inputFieldChecker = async (targetElement, imageElement, imageLabel, e)=> {
     }
   else {
     if(targetElement.value.length>5) imageElement.classList.add("available") 
-    else if (targetElement.value.length<=5&&targetElement.value.length>=1) {
-      imageElement.classList.add("tooshort")
-      imageElement.addEventListener("mouseleave" , ()=> {imageLabel.style.visibility="hidden" })
-      imageElement.addEventListener("mouseenter" , ()=> {
-        imageLabel.style.visibility="visible"
-        imageLabel.style.color="grey" 
-        imageLabel.innerHTML="too short"
-    })
-  }
+    else if (targetElement.value.length<=5&&targetElement.value.length>=1) changeColors(imageElement, imageLabel, "tooshort", "grey", "too short" )
 }
+}
+
+
+const placeholderMovement = (element, placeholder)=> {
+
+  element.addEventListener("keyup", e=>{
+    if(e.target.value.length>0) {
+      placeholder.classList.add(`signupBox__placeholder--active`)
+      placeholder.classList.remove(`signupBox__placeholder--inactive`)
+    }
+    else {
+      placeholder.classList.remove(`signupBox__placeholder--active`)
+      placeholder.classList.add(`signupBox__placeholder--inactive`)
+    }
+  })
+
+  element.addEventListener("keydown", e=>{
+    const key = e.key
+    if(e.target.value.length === 1) {
+      if(element.selectionStart === 0 && key === "Delete" || element.selectionStart === 1 && key === "Backspace")  
+      element.classList.remove("signupBox__inputText--active")   
+    }
+    else {
+      element.classList.add("signupBox__inputText--active")
+    }
+  })
+
 }
